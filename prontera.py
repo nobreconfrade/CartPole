2949c9d29587af83ff99de6c51ad79ef93bcf164
Your Name <willpereirabr@gmail.com>
2018-07-16 10:14:29 -0300

    some experimentation

--- prontera.py
+++ prontera.py
@@ -2,11 +2,13 @@ import gym
 import time
 
 env = gym.make('CartPole-v0')
-env.reset()
-for _ in range(1000):
-    if done == True:
-        print('resetado')
-        env.reset()
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
-    time.sleep(0.05)
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        # action = env.action_space.sample()
+        observation, reward, done, info = env.step(1)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
